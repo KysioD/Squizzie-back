8b9b2e49efb04d0ae837a83fd0128ef57be395e3
Hugom <hugo.malaquin@gmail.com>
2023-09-22 12:04:38 +0200

    Add question controller

--- src/main/java/fr/kysio/squeezie/controllers/QuestionController.java
+++ src/main/java/fr/kysio/squeezie/controllers/QuestionController.java
@@ -1,6 +1,10 @@
 package fr.kysio.squeezie.controllers;
 
+import fr.kysio.squeezie.logic.dtos.QuestionDto;
+import fr.kysio.squeezie.services.QuestionsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/question")
 @RequiredArgsConstructor
 public class QuestionController {
+
+    private final QuestionsService questionsService;
+
+    @GetMapping("/next/{quizzId}/{username}")
+    public QuestionDto getNextQuestion(@PathVariable Integer quizzId, @PathVariable String username) {
+        return questionsService.getNextQuestion(quizzId, username);
+    }
+
 }
