a1d843e2f97cb079e41e38098aaf8689a6ffe288
Hugom <hugo.malaquin@gmail.com>
2023-09-22 09:38:13 +0200

    Setup springboot backbone

---
+++ src/main/java/fr/kysio/squeezie/SqueezieApplication.java
@@ -0,0 +1,13 @@
+package fr.kysio.squeezie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqueezieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqueezieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/kysio/squeezie/SqueezieApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.kysio.squeezie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqueezieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
